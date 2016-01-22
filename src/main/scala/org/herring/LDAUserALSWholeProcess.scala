7c3ac33fd4fcb0e93708203e668a9e284e64e003
Hyunje Jo <retriever89@gmail.com>
2016-01-23 07:39:08 +0900

    오류 수정

--- src/main/scala/org/herring/LDAUserALSWholeProcess.scala
+++ src/main/scala/org/herring/LDAUserALSWholeProcess.scala
@@ -7,6 +7,7 @@ import org.apache.spark.mllib.clustering.{DistributedLDAModel, LDA}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
 import org.apache.spark.sql.Row
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkContext, SparkConf}
 
 import scala.collection.mutable.ArrayBuffer
@@ -16,16 +17,21 @@ import scala.collection.mutable.ArrayBuffer
   * 이 형태의 추천 과정을 처음부터 끝까지 수행하는 클래스.
   * 수행하기 전에 PreProcessing 클래스가 수행된 상태여야 하며, 결과가 저장될 폴더는 지워진 상태여야 한다.
   *
+  * spark-submit --master master_URL --class org.herring.LDAUserALSWholeProcess --packages com.databricks:spark-csv_2.10:1.3.0 target/yes24-1.0-allinone.jar
+  *
   * @author hyunje
   * @since 1/14/16, 2:46 PM.
   */
 object LDAUserALSWholeProcess {
     val minLength = 2
     val rank = 10
-    val numIterations = 10
+    val numRecIterations = 10
     val alpha = 0.01
     val lambda = 0.01
-    val k: Int = 3
+    val topClusterNum: Int = 3
+    val numTopics = 20
+    val maxLDAIters: Int = 150
+
 
     def main(args: Array[String]) {
         //Spark Context 생성.
@@ -33,11 +39,14 @@ object LDAUserALSWholeProcess {
             .setAppName("Yes24 LDA + User Clustering + ALS")
             .set("spark.default.parallelism", "4")
         val sc = new SparkContext(conf)
-        sc.setCheckpointDir("/yes24/data/checkpoint")
+//        sc.setCheckpointDir("hdfs://localhost:8020/yes24/data/checkpoint")
+
 
         //==============================================
         //        PreProcessing 결과를 불러오는 과정
         //==============================================
+
+        println("전처리 결과 로드")
 
         //해당 카테고리만 필터링 된 결과
         val filters = sc.objectFile[Row]("/yes24/data/filters")
@@ -72,11 +81,15 @@ object LDAUserALSWholeProcess {
             val intro = if (tokens.length == 5) tokens.apply(4) else title
             (bookId, intro)
         }
+        println("전처리 결과 로드 종료")
+        println()
 
 
         //==============================================
         //        LDA를 수행하기 위한 데이터 준비과정
         //==============================================
+
+        println("LDA를 수행하기 위한 데이터 준비")
 
         //책 소개 형태소 분석
         val bookStemmed = bookData.map { case (id, intro) =>
@@ -111,7 +124,7 @@ object LDAUserALSWholeProcess {
             val result = temp.fold(Seq[String]()) { (a, b) => a ++ b }
             result
         }
-        userNouns.cache()
+        userNouns.persist(StorageLevel.MEMORY_AND_DISK)
 
         //LDA와 클러스터링에 사용될 사용자별 Document 생성
         val documents = userNouns.map { case (id, nouns) =>
@@ -124,16 +137,19 @@ object LDAUserALSWholeProcess {
             }
             (id.toLong, Vectors.sparse(bWordMap.value.size, counts.toSeq))
         }
-        documents.cache()
+        documents.persist(StorageLevel.MEMORY_AND_DISK)
+        println("LDA를 수행하기 위한 데이터 준비 완료")
+        println()
 
 
         //==============================================
         //        LDA를 이용한 클러스터링을 수행하는 과정
         //==============================================
 
+        println("LDA 수행")
+
         //LDA 수행
-        val numTopics = 20
-        val lda = new LDA().setK(numTopics).setMaxIterations(150).setCheckpointInterval(10).setOptimizer("em")
+        val lda = new LDA().setK(numTopics).setMaxIterations(maxLDAIters).setCheckpointInterval(10).setOptimizer("em")
         val preLdaModel = lda.run(documents)
         //일단 수행한 LDA 모델 저장
         val modelName = "/yes24/ldamodel/all"
@@ -144,28 +160,27 @@ object LDAUserALSWholeProcess {
         documents.unpersist()
         userNouns.unpersist()
 
-
-
         //LDA 수행 결과 기반의 클러스터링 수행
-        val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = 10)
-        val userTopicDistribution = ldaModel.topTopicsPerDocument(k)
+        val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = topClusterNum)
+        val userTopicDistribution = ldaModel.topTopicsPerDocument(topClusterNum)
         val clusteringResult = userTopicDistribution.flatMap { case (uid, tids, tweights) =>
             tids.map(t => (uid, t)).zip(tweights).map(t => (t._1._1, t._1._2, t._2))
         }
         val userCluster = clusteringResult.map(i => (i._1, i._2))
         val bUserCluster = sc.broadcast(userCluster.collect())
-        userCluster.cache()
+        userCluster.persist(StorageLevel.MEMORY_AND_DISK)
         val groupedUserCluster = userCluster.groupByKey()
-
-        println("Num of Topics: " + ldaModel.k)
+        println("LDA 수행 완료")
+        println()
 
 
         //==============================================
         //        각 클러스터별로 추천을 수행하기 위한 준비
         //==============================================
 
-        //각 클러스터별로 추천을 수행하기 위한 데이터 Filtering
+        println("각 클러스터별 추천 수행 준비")
 
+        //각 클러스터별로 추천을 수행하기 위한 데이터 Filtering
         // TODO: Join으로 해결하는 것이 효과적인가, 아니면 User-Ratings 의 Map을 구성한 후 Broadcasting 하여 처리하는것이 나은가?
         //ratingForEachCluster: 각 클러스터별로 (클러스터 Id, Array[Ratings])
         val ratingForEachCluster = userItem.map(i => (i._1.toLong, Rating(i._1.toInt, i._2.toInt, 1.0))).groupByKey()
@@ -177,44 +192,47 @@ object LDAUserALSWholeProcess {
               clusters.map(cnum => (cnum, ratings))
           }.groupByKey().mapValues(_.reduce((a, b) => a ++ b)).mapValues(_.toArray)
 
-        ratingForEachCluster.cache()
-
-        println(ratingForEachCluster.take(1).head)
-        println("Num of Cluster: " + ratingForEachCluster.count())
+        ratingForEachCluster.persist(StorageLevel.MEMORY_AND_DISK)
+        println("각 클러스터별 추천 수행 준비 완료")
+        println()
 
 
         //==============================================
         //             각 클러스터별로 추천을 수행
         //==============================================
 
+        println("각 클러스터별 추천 수행")
+
         //각 클러스터별로 ALS 수행
         val numOfClusters = ratingForEachCluster.count().toInt
         val recResult = new ArrayBuffer[(Int, Int, Array[Rating])]() //(Cluster#, uId, Rec)
 
         for (cnum <- 0 until numOfClusters) {
-            println("Cluster: " + cnum)
             val ratings = ratingForEachCluster.filter(_._1 == cnum).take(1).head._2
             val ratingsRdd = sc.parallelize(ratings)
-            ratingsRdd.cache()
+            ratingsRdd.persist(StorageLevel.MEMORY_AND_DISK)
 
-            val model: MatrixFactorizationModel = ALS.trainImplicit(ratingsRdd, rank, numIterations, lambda, alpha)
+            val model: MatrixFactorizationModel = ALS.trainImplicit(ratingsRdd, rank, numRecIterations, lambda, alpha)
             val users = bUserCluster.value.filter(_._2 == cnum).map(_._1.toInt)
 
             for (uid <- users) {
                 val rec = model.recommendProducts(uid, rank)
                 recResult += ((cnum, uid, rec))
             }
-
             ratingsRdd.unpersist()
         }
-        val recResultRdd = sc.parallelize(recResult)
-        recResultRdd.cache()
+        val recResultRdd = sc.parallelize(recResult).map(l => (l._2, l._1, l._3))
+        recResultRdd.persist(StorageLevel.MEMORY_AND_DISK)
+
+        println("각 클러스터별 추천 수행 완료")
+        println()
 
 
         //==============================================
         //            각 사용자별로 최종 추천 계산
         //==============================================
 
+        println("각 사용자별 최종 추천 계산")
         //추천 결과와 클러스터별 가중치를 이용한 추천 계산
         val userDistSum = userTopicDistribution.map { dist => (dist._1.toInt, dist._3.sum) }.collectAsMap()
         val bUserDistSum = sc.broadcast(userDistSum)
@@ -239,8 +257,11 @@ object LDAUserALSWholeProcess {
                 (oId, title, r.rating)
             }
         }
+        println("각 사용자별 최종 추천 계산 완료")
+        println()
 
         //추천 결과 저장
+        println("추천 결과 저장")
         filteredRecommendationResult.map { r =>
             r._1 + Console.RED + r._2 + Console.RED + r._3
         }.coalesce(1).saveAsTextFile("/yes24/result/final")
