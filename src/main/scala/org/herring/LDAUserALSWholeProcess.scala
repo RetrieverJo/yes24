c6bd0a321499b4256138e2045709ba5afc30fcc0
Hyunje Jo <retriever89@gmail.com>
2016-01-25 19:29:22 +0900

    함수 추출과정

--- src/main/scala/org/herring/LDAUserALSWholeProcess.scala
+++ src/main/scala/org/herring/LDAUserALSWholeProcess.scala
@@ -6,10 +6,12 @@ import com.twitter.penguin.korean.util.KoreanPos
 import org.apache.spark.mllib.clustering.{DistributedLDAModel, LDA}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkContext, SparkConf}
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 /**
@@ -32,6 +34,11 @@ object LDAUserALSWholeProcess {
     val numTopics = 20
     val maxLDAIters: Int = 150
 
+    val filtersPath: String = "/yes24/data/filters"
+    val uidIndexPath: String = "/yes24/data/uidIndex"
+    val bookWithIdPath: String = "/yes24/data/bookWithId"
+    val bookDataPath: String = "/yes24/data/bookData"
+    val delimiter: String = "\\u001B\\[31m"
 
     def main(args: Array[String]) {
         //Spark Context 생성.
@@ -39,49 +46,20 @@ object LDAUserALSWholeProcess {
             .setAppName("Yes24 LDA + User Clustering + ALS")
             .set("spark.default.parallelism", "4")
         val sc = new SparkContext(conf)
-//        sc.setCheckpointDir("hdfs://localhost:8020/yes24/data/checkpoint")
-
 
         //==============================================
         //        PreProcessing 결과를 불러오는 과정
         //==============================================
+        println("Load pre-processing result")
 
-        println("전처리 결과 로드")
+        val (filters, users, items, bookData) = loadPreprocessing(sc)
 
-        //해당 카테고리만 필터링 된 결과
-        val filters = sc.objectFile[Row]("/yes24/data/filters")
-
-        //사용자 ID의 인덱싱 결과
-        val users = sc.textFile("/yes24/data/uidIndex").map { line =>
-            val tokens = line.split("\\u001B\\[31m")
-            val nid = tokens.apply(0)
-            val oid = tokens.apply(1)
-            (oid, nid)
-        }
         val userOIdNId = users.collectAsMap()
         val userNIdOId = users.map(_.swap).collectAsMap()
-        //아이템 정보 로드
-        val items = sc.textFile("/yes24/data/bookWithId").map { line =>
-            val tokens = line.split("\\u001B\\[31m")
-            val id = tokens.apply(0)
-            val title = tokens.apply(1).trim
-            //            val isbn = tokens.applyOrElse(2, "-")
-            (title, id)
-        }
         val bookTitleId = items.collectAsMap()
         val bookIdTitle = items.map(_.swap).collectAsMap()
 
-        //책 정보(소개 정보 로드)
-        val bookData = sc.textFile("/yes24/data/bookData").map { line =>
-            val tokens = line.split("\\u001B\\[31m")
-            val bookId = tokens.apply(0)
-            val title = tokens.apply(1)
-            //            val isbn = tokens.apply(2)
-            //            val yes24id = tokens.apply(3)
-            val intro = if (tokens.length == 5) tokens.apply(4) else title
-            (bookId, intro)
-        }
-        println("전처리 결과 로드 종료")
+        println("Finished loading pre-processing result")
         println()
 
 
@@ -89,7 +67,7 @@ object LDAUserALSWholeProcess {
         //        LDA를 수행하기 위한 데이터 준비과정
         //==============================================
 
-        println("LDA를 수행하기 위한 데이터 준비")
+        println("Preparing LDA")
 
         //책 소개 형태소 분석
         val bookStemmed = bookData.map { case (id, intro) =>
@@ -124,11 +102,10 @@ object LDAUserALSWholeProcess {
             val result = temp.fold(Seq[String]()) { (a, b) => a ++ b }
             result
         }
-        userNouns.persist(StorageLevel.MEMORY_AND_DISK)
 
         //LDA와 클러스터링에 사용될 사용자별 Document 생성
         val documents = userNouns.map { case (id, nouns) =>
-            val counts = new scala.collection.mutable.HashMap[Int, Double]()
+            val counts = new mutable.HashMap[Int, Double]()
             nouns.foreach { term =>
                 if (bWordMap.value.contains(term)) {
                     val idx = bWordMap.value(term)
@@ -137,8 +114,11 @@ object LDAUserALSWholeProcess {
             }
             (id.toLong, Vectors.sparse(bWordMap.value.size, counts.toSeq))
         }
+
+        userNouns.persist(StorageLevel.MEMORY_AND_DISK)
         documents.persist(StorageLevel.MEMORY_AND_DISK)
-        println("LDA를 수행하기 위한 데이터 준비 완료")
+
+        println("Finished preparing LDA")
         println()
 
 
@@ -146,7 +126,7 @@ object LDAUserALSWholeProcess {
         //        LDA를 이용한 클러스터링을 수행하는 과정
         //==============================================
 
-        println("LDA 수행")
+        println("Run LDA")
 
         //LDA 수행
         val lda = new LDA().setK(numTopics).setMaxIterations(maxLDAIters).setCheckpointInterval(10).setOptimizer("em")
@@ -160,17 +140,12 @@ object LDAUserALSWholeProcess {
         documents.unpersist()
         userNouns.unpersist()
 
-        //LDA 수행 결과 기반의 클러스터링 수행
-        val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = topClusterNum)
-        val userTopicDistribution = ldaModel.topTopicsPerDocument(topClusterNum)
-        val clusteringResult = userTopicDistribution.flatMap { case (uid, tids, tweights) =>
-            tids.map(t => (uid, t)).zip(tweights).map(t => (t._1._1, t._1._2, t._2))
-        }
-        val userCluster = clusteringResult.map(i => (i._1, i._2))
+        val (userTopicDistribution, clusteringResult, userCluster) = ldaResultBasedClustering(ldaModel)
+
         val bUserCluster = sc.broadcast(userCluster.collect())
         userCluster.persist(StorageLevel.MEMORY_AND_DISK)
         val groupedUserCluster = userCluster.groupByKey()
-        println("LDA 수행 완료")
+        println("Finished running LDA")
         println()
 
 
@@ -180,17 +155,7 @@ object LDAUserALSWholeProcess {
 
         println("각 클러스터별 추천 수행 준비")
 
-        //각 클러스터별로 추천을 수행하기 위한 데이터 Filtering
-        // TODO: Join으로 해결하는 것이 효과적인가, 아니면 User-Ratings 의 Map을 구성한 후 Broadcasting 하여 처리하는것이 나은가?
-        //ratingForEachCluster: 각 클러스터별로 (클러스터 Id, Array[Ratings])
-        val ratingForEachCluster = userItem.map(i => (i._1.toLong, Rating(i._1.toInt, i._2.toInt, 1.0))).groupByKey()
-          .join(groupedUserCluster)
-          .flatMap { uidRatingCluster =>
-              val uid = uidRatingCluster._1
-              val ratings = uidRatingCluster._2._1.toSeq
-              val clusters = uidRatingCluster._2._2
-              clusters.map(cnum => (cnum, ratings))
-          }.groupByKey().mapValues(_.reduce((a, b) => a ++ b)).mapValues(_.toArray)
+        val ratingForEachCluster = prepareRecommendation(userItem, groupedUserCluster)
 
         ratingForEachCluster.persist(StorageLevel.MEMORY_AND_DISK)
         println("각 클러스터별 추천 수행 준비 완료")
@@ -222,6 +187,8 @@ object LDAUserALSWholeProcess {
             ratingsRdd.unpersist()
         }
         val recResultRdd = sc.parallelize(recResult).map(l => (l._2, l._1, l._3))
+
+
         recResultRdd.persist(StorageLevel.MEMORY_AND_DISK)
 
         println("각 클러스터별 추천 수행 완료")
@@ -268,5 +235,64 @@ object LDAUserALSWholeProcess {
 
         ratingForEachCluster.unpersist()
         recResultRdd.unpersist()
+    }
+
+    def prepareRecommendation(userItem: RDD[(String, String)], groupedUserCluster: RDD[(Long, Iterable[Int])]): RDD[(Int, Array[Rating])] = {
+        //각 클러스터별로 추천을 수행하기 위한 데이터 Filtering
+        // TODO: Join으로 해결하는 것이 효과적인가, 아니면 User-Ratings 의 Map을 구성한 후 Broadcasting 하여 처리하는것이 나은가?
+        //ratingForEachCluster: 각 클러스터별로 (클러스터 Id, Array[Ratings])
+        val ratingForEachCluster = userItem.map(i => (i._1.toLong, Rating(i._1.toInt, i._2.toInt, 1.0))).groupByKey()
+          .join(groupedUserCluster)
+          .flatMap { uidRatingCluster =>
+              val uid = uidRatingCluster._1
+              val ratings = uidRatingCluster._2._1.toSeq
+              val clusters = uidRatingCluster._2._2
+              clusters.map(cnum => (cnum, ratings))
+          }.groupByKey().mapValues(_.reduce((a, b) => a ++ b)).mapValues(_.toArray)
+        ratingForEachCluster
+    }
+
+    def ldaResultBasedClustering(ldaModel: DistributedLDAModel): (RDD[(Long, Array[Int], Array[Double])], RDD[(Long, Int, Double)], RDD[(Long, Int)]) = {
+        //LDA 수행 결과 기반의 클러스터링 수행
+        val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = topClusterNum)
+        val userTopicDistribution = ldaModel.topTopicsPerDocument(topClusterNum)
+        val clusteringResult = userTopicDistribution.flatMap { case (uid, tids, tweights) =>
+            tids.map(t => (uid, t)).zip(tweights).map(t => (t._1._1, t._1._2, t._2))
+        }
+        val userCluster = clusteringResult.map(i => (i._1, i._2)).groupByKey().mapValues(clusters => clusters.toList)
+        (userTopicDistribution, clusteringResult, userCluster)
+    }
+
+    def loadPreprocessing(sc: SparkContext): (RDD[Row], RDD[(String, String)], RDD[(String, String)], RDD[(String, String)]) = {
+        //해당 카테고리만 필터링 된 결과
+        val filters = sc.objectFile[Row](filtersPath)
+
+        //사용자 ID의 인덱싱 결과
+        val users = sc.textFile(uidIndexPath).map { line =>
+            val tokens = line.split(delimiter)
+            val nid = tokens.apply(0)
+            val oid = tokens.apply(1)
+            (oid, nid)
+        }
+        //아이템 정보 로드
+        val items = sc.textFile(bookWithIdPath).map { line =>
+            val tokens = line.split(delimiter)
+            val id = tokens.apply(0)
+            val title = tokens.apply(1).trim
+            //            val isbn = tokens.applyOrElse(2, "-")
+            (title, id)
+        }
+
+        //책 정보(소개 정보 로드)
+        val bookData = sc.textFile(bookDataPath).map { line =>
+            val tokens = line.split(delimiter)
+            val bookId = tokens.apply(0)
+            val title = tokens.apply(1)
+            //            val isbn = tokens.apply(2)
+            //            val yes24id = tokens.apply(3)
+            val intro = if (tokens.length == 5) tokens.apply(4) else title
+            (bookId, intro)
+        }
+        (filters, users, items, bookData)
     }
 }
